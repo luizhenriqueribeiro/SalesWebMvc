9ab5e146f483f2d64f19987cb1d33ef78f6d58f1
luizhenriqueribeiro <luizribeiro266@gmail.com>
2023-09-24 11:47:55 -0300

    Created Project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SalesWEbMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
